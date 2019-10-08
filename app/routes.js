36cab576b17e552a3115e6acc73b53048d90bb70
Richard Race <richard.race@madetech.com>
2019-10-08 14:06:17 +0100

    Allow specific IDs to be used to view cases

--- app/routes.js
+++ app/routes.js
@@ -7,4 +7,9 @@ router.get('/case-details', function(req, res) {
   res.render('case-details/index', cases.first())
 })
 
+router.get('/case-details/:id', function(req, res) {
+  const caseObject = cases.find(req.params.id)
+  res.render('case-details/index', caseObject)
+})
+
 module.exports = router
