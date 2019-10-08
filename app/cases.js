c29aadb464bcd61641a12c832c81bcc9e5b5c241
Richard Race <richard.race@madetech.com>
2019-10-08 14:05:45 +0100

    Add extra data to the Cases Factory

--- app/cases.js
+++ app/cases.js
@@ -5,6 +5,10 @@ class Cases {
   constructor() {
     this.records = []
 
+    this.lastActions = ['SMS', 'Letter 1', 'Letter 2', 'None']
+    this.agreements = ['Suspended', 'Breached', 'Live', 'None']
+    this.paused = ['Yes', 'No']
+
     for (let i = 0; i < 50; i++) {
       this.records.push(this.newRecord())
     }
@@ -12,6 +16,14 @@ class Cases {
 
   newRecord() {
     const startDate = faker.date.between('1960-01-01', '2019-01-01')
+    const agreement = this.agreements[Math.floor(Math.random() * this.agreements.length)]
+    const lastAction = this.lastActions[Math.floor(Math.random() * this.lastActions.length)]
+    const lastActionDate = faker.date.recent(365) // 365 Days
+    let lastActionString = lastAction;
+
+    if (lastAction !== 'None') {
+      lastActionString = `${lastActionString}\n${moment(lastActionDate).format('Do MMM')}`
+    }
 
     return {
       name: `${faker.name.firstName()} ${faker.name.lastName()}`,
@@ -23,11 +35,18 @@ class Cases {
       assignedTo: 'Test (Base User)',
       accontBalance: faker.finance.amount(0, 10000, 2, '£'),
       address: faker.address.streetAddress(),
+      lastAction: lastActionString,
+      agreement: agreement,
+      paused: this.paused[Math.floor(Math.random() * this.paused.length)]
     }
   }
 
   first() {
     return this.records[0]
+  }
+
+  find(id) {
+    return this.records.find(c => c.ref === id)
   }
 }
 
